e6d8262659b4f21ac2321d740f3879422ef35dd0
Paul Merideth <plmerideth@gmail.com>
2015-05-13 12:46:59 -0600

    Initial commit to repository.

---
+++ LargeAndSpacious/src/largeandspacious/LargeAndSpacious.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package largeandspacious;
+
+import largeandspacious.model.Player;
+
+/**
+ *
+ * @author Paul Merideth
+ */
+public class LargeAndSpacious {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Player playerOne = new Player();
+        
+        playerOne.setName("Paul Merideth");
+        playerOne.setBestScore(123.45);
+        
+        String playerInfo = playerOne.toString();
+        System.out.println(playerInfo);
+    }
+    
+}
---
+++ LargeAndSpacious/src/largeandspacious/model/Player.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package largeandspacious.model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Paul Merideth
+ */
+public class Player implements Serializable
+{
+    private String name;
+    private double bestScore;
+
+    public Player() {
+    }
+
+    
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBestScore() {
+        return bestScore;
+    }
+
+    public void setBestScore(double bestScore) {
+        this.bestScore = bestScore;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" + "name=" + name + ", bestScore=" + bestScore + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 37 * hash + (int) (Double.doubleToLongBits(this.bestScore) ^ (Double.doubleToLongBits(this.bestScore) >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Player other = (Player) obj;
+        if (Double.doubleToLongBits(this.bestScore) != Double.doubleToLongBits(other.bestScore)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+}
