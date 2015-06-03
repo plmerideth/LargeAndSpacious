c2ae3c58528d737ead66461cd52756fa11a65c82
Paul Merideth <plmerideth@gmail.com>
2015-06-03 01:10:47 -0600

    Added Challenges.java

---
+++ LargeAndSpacious/src/largeandspacious/control/ProgramControl.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package largeandspacious.control;
+
+import largeandspacious.LargeAndSpacious;
+import largeandspacious.model.Player;
+
+/**
+ *
+ * @author Paul Merideth
+ */
+public class ProgramControl
+{
+
+    public static Player createPlayer(String playersName)
+    {
+        if( playersName == null)
+            return null;
+        
+        Player player = new Player();
+        player.setName(playersName);
+        
+        LargeAndSpacious.setPlayer(player);
+        
+        return player;
+    }
+}
