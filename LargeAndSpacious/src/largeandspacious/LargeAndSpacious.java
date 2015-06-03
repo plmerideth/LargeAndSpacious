71b3192db4fe393d8244f8b476da4cf38d71b6a7
Paul Merideth <plmerideth@gmail.com>
2015-06-03 01:12:09 -0600

    Added Challenges.java

--- LargeAndSpacious/src/largeandspacious/LargeAndSpacious.java
+++ LargeAndSpacious/src/largeandspacious/LargeAndSpacious.java
@@ -17,103 +17,33 @@ import largeandspacious.model.QuestionScene;
 import largeandspacious.model.Questions;
 import largeandspacious.model.Item;
 import largeandspacious.model.Location;
+import largeandspacious.view.StartProgramView;
 
 /**
  *
  * @author Paul Merideth
  */
-public class LargeAndSpacious {
+public class LargeAndSpacious
+{
+    private static Game currentGame = null;
+    private static Player player = null;
 
+    public static void setCurrentGame(Game currentGame) {
+        LargeAndSpacious.currentGame = currentGame;
+    }
+
+    public static void setPlayer(Player player) {
+        LargeAndSpacious.player = player;
+    }
+    
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        Player playerOne = new Player();
-        
-        playerOne.setName("Paul Merideth");
-        playerOne.setBestScore(123.45);
-        
-        String playerInfo = playerOne.toString();
-        System.out.println(playerInfo);
-        
-        Game gameOne = new Game();
-        gameOne.setGameID(1);
-        gameOne.setRemainingTime(10);
-        gameOne.setTotalTime(15);
-        
-        String gameInfo = gameOne.toString();
-        System.out.println(gameInfo);
-        
-        Map firstMap = new Map();
-        firstMap.setRow(3);
-        firstMap.setColumn(5);
-        
-        String myMap = firstMap.toString();
-        System.out.println(myMap);
-        
-        QuestionScene questScene = new QuestionScene();
-        questScene.setSceneTypeID(2);
-        questScene.setQuestionID(7);
-        questScene.setItemID(2);
-        questScene.setDescription("Question about Lehi's dream.");
-        
-        String myQuestScene = questScene.toString();
-        System.out.println(myQuestScene);
-        
-        Questions quest = new Questions();
-        quest.setQuestionsID(9);
-        quest.setQuestion("What does the iron rod represent in Lehi's dream?");
-        quest.setAnswer("The word of God.");
-        
-        String myQuest = quest.toString();
-        System.out.println(myQuest);
-        
-        Item myItem = new Item();
-        myItem.setItemID(4);
-        myItem.setName("Obedience");
-        myItem.setDescription("Your level of obedience");
-        myItem.setValue(3);
-        myItem.setItemCount(1);
-        
-        String haveItems = myItem.toString();
-        System.out.println(haveItems);
-        
-        Location mapLocation = new Location();
-        mapLocation.setRow(3);
-        mapLocation.setCol(2);
-        mapLocation.setSceneTypeId(4);
-        String locationInfo = mapLocation.toString();
-        System.out.println(locationInfo);
-        
-        ChallengeScene challengeScene = new ChallengeScene();
-        challengeScene.setSceneTypeID(32);
-        challengeScene.setChallengeID(45);
-        challengeScene.setItemID(56);
-        challengeScene.setDescription("This is challenge scene 45 and is not for the faint of heart!");
-        String challengeSceneInfo = challengeScene.toString();
-        System.out.println(challengeSceneInfo);
-        
-        CombinationScene combinationScene = new CombinationScene();
-        combinationScene.setSceneTypeID(99);
-        combinationScene.setQuestionID(88);
-        combinationScene.setChallengeID(77);
-        combinationScene.setItemID(66);
-        combinationScene.setDescription("This is the combination scene.  Double trouble!");
-        String combinationSceneInfo = combinationScene.toString();
-        System.out.println(combinationSceneInfo);
-        
-        Challenges challenges = new Challenges();
-        challenges.setChallengeID(200);
-        challenges.setChallengeValue(15);
-        challenges.setDescription("The Mists of Darkness will destroy you if you're not careful!");
-        String challengesInfo = challenges.toString();
-        System.out.println(challengesInfo);
-        
-        Actor actor = new Actor();
-        actor.setActorID(2);
-        actor.setActorName("Nephi");
-        String actorInfo = actor.toString();
-        System.out.println(actorInfo);
+    public static void main(String[] args)
+    {
+        //Create StartProgramView and start the program
+        StartProgramView startProgramView = new StartProgramView();
+        startProgramView.startProgram();
     }
     
 }
