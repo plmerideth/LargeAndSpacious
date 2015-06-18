4eac97eebd7be6aefb1c2b43744cac396067e80a
Paul Merideth <plmerideth@gmail.com>
2015-06-18 13:34:55 -0600

    Added Challenges.java

---
+++ LargeAndSpacious/src/largeandspacious/view/ViewInterface.java
@@ -0,0 +1,13 @@
+
+package largeandspacious.view;
+
+/**
+ *
+ * @author Paul Merideth
+ */
+public interface ViewInterface
+{
+    public void display();
+    public String getInput();
+    public boolean doAction(Object obj);
+}
