7cae8fa7f0c8493900821ff4d6301679405db2fc
Paul Merideth <plmerideth@gmail.com>
2015-06-18 13:35:41 -0600

    Added Challenges.java

--- LargeAndSpacious/src/largeandspacious/view/StartProgramView.java
+++ LargeAndSpacious/src/largeandspacious/view/StartProgramView.java
@@ -27,7 +27,7 @@ public class StartProgramView
             
             //Display the main menu
             MainMenuView mainMenu = new MainMenuView();
-            mainMenu.displayMenu();
+            mainMenu.display();
         //END
     }
 
